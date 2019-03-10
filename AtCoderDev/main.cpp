00550d4305c4892afaaec2fe6e8348df5e1a4a6d
Hiroki_KOJIMA <h.k.psi.mot@gmail.com>
2019-03-10 10:38:55 +0900

    Initial Commit

---
+++ AtCoderDev/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AtCoderDev
+//
+//  Created by Hiroki Kojima on 2019/03/10.
+//  Copyright © 2019 Hiroki Kojima. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
