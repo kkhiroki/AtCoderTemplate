dcd49bce6d76259e934ad25610ca37e4c431db97
Hiroki_KOJIMA <h.k.psi.mot@gmail.com>
2019-07-01 22:25:36 +0900

    remove header

--- tips_module.cpp
+++ tips_module.cpp
@@ -1,13 +1,3 @@
-//
-//  tips_module.cpp
-//  AtCoderDev
-//
-//  Created by Hiroki Kojima on 2019/04/29.
-//  Copyright © 2019 Hiroki Kojima. All rights reserved.
-//
-
-#include <stdio.h>
-
 ll nCk(ll n, ll k){
     if(n < k) return 0;
     return fact[n] * (finv[k]*finv[n-k]%mod) %mod;
