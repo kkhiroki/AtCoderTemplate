fe2bc123ef4f0ad7f8e381cf2b80a0912bacbea1
Hiroki_KOJIMA <h.k.psi.mot@gmail.com>
2019-07-20 20:42:55 +0900

    remove work directory if it exists

--- scripts/dl_contest.sh
+++ scripts/dl_contest.sh
@@ -1,6 +1,7 @@
 contest=$1
 
 work=/Users/hiroki/AtCoderDev/work
+rm -rf $work
 mkdir $work
 cd $work
 
@@ -8,7 +9,7 @@ url_base="https://atcoder.jp/contests/${contest}/tasks/${contest}"
 
 echo $url_base
 
-for t in a b c d e f g h; do
+for t in a b c d e f; do
 	url="${url_base}_${t}"
 	echo "now downloading... ${url}"
 	oj dl $url
