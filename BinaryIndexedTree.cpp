64e644f71c61e6b9f7c93eba0ca2bee55ecc4c05
Hiroki_KOJIMA <h.k.psi.mot@gmail.com>
2019-08-13 22:20:06 +0900

    add binary indexed tree

---
+++ BinaryIndexedTree.cpp
@@ -0,0 +1,17 @@
+template<typename T>
+struct BinaryIndexedTree{
+    vector<T> data;
+    BinaryIndexedTree(ll sz){
+        data.assign(++sz, 0);
+    }
+    
+    T sum(ll k){
+        T ret = 0;
+        for(++k; k>0; k -= k&-k) ret += data[k];
+        return ret;
+    }
+    
+    void add(int k, T x){
+        for(++k; k < data.size(); k += k&-k) data[k] += x;
+    }
+};
